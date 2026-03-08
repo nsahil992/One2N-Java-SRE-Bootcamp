5c12b55bdbcc75b46795e6595090645d014f4a05
Sahil Naik <nsahil992@gmail.com>
2026-03-08 16:03:02 +0530

    feat: add healthcheck with seperate Health Controller

---
+++ src/main/java/com/example/student_api/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.student_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HealthController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthcheck() {
+        return ResponseEntity.ok("UP");
+    }
+}
